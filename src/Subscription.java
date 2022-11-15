414e9c38db033192f0f12ae1a70f547ee3314c1a
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2022-11-14 22:20:30 -0500

    created files and method

---
+++ src/Subscription.java
@@ -0,0 +1,12 @@
+public class Subscription {
+
+    private String strSubscriptionType;
+    private int intAmountUsers;
+    private int intBalance;
+
+    public String getSubscriptionType() {
+        return strSubscriptionType;
+    }
+
+    
+}
