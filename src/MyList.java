b5c2ff1d0d001177e094c7c56e8b91eb5a70fdef
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2022-11-21 21:33:37 -0500

    added methods in mylist class

--- src/MyList.java
+++ src/MyList.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
+import javax.print.attribute.standard.Media;
+
 public class MyList {
+
+    private ArrayList<Media> list;
+    private int maxShow;
+
+    public MyList(ArrayList<Media> list){
+        this.list = list;
+    }
+
+
     
 }
