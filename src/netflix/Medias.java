10f86705aea3554b30251634a18d61825a056062
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2022-11-25 00:31:08 -0500

    instance variables and constructor

---
+++ src/netflix/Medias.java
@@ -0,0 +1,25 @@
+package netflix;
+
+public abstract class Medias {
+    
+    // Instance variables 
+    private String strMediaName;
+    private String strDirectorName;
+    private String strGenre;
+    private int intFilmYear;
+
+    /**
+     * set constructor 
+     * @param strMediaName: displays the media name 
+     * @param strDirectorName: displays the director name 
+     * @param strGenre: displays the genre 
+     * @param intFilmYear: displays film year
+     */
+    public Medias(String strMediaName, String strDirectorName, String strGenre, int intFilmYear) {
+
+        this.strMediaName = strMediaName;
+        this.strDirectorName = strDirectorName;
+        this.strGenre = strGenre;
+        this.intFilmYear = intFilmYear;
+    }
+}
