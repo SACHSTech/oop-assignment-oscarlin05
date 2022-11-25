5d4bb359a4048d536eaa6bb331f5c61dca208f52
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2022-11-25 00:29:01 -0500

    comments

--- src/netflix/Movies.java
+++ src/netflix/Movies.java
@@ -5,7 +5,14 @@ public class Movies extends Medias {
     // Instance variable
     private boolean blnAgeRestriction;
 
-    // set constructor 
+    /**
+     * set constructor 
+     * @param strMediaName: displays media name 
+     * @param strDirectorName: displays director name 
+     * @param strGenre: displays genre 
+     * @param intFilmYear: displays film year 
+     * @param ageRestriction: displays age restriction based on users age 
+     */
     public Movies(String strMediaName, String strDirectorName, String strGenre, int intFilmYear, boolean ageRestriction) {
         super(strMediaName, strDirectorName, strGenre, intFilmYear);
     }
