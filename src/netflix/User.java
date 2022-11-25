7cc698777be1d5fa32ad0df0a310e1a4d49269a8
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2022-11-24 20:33:00 -0500

    constructor added

--- src/netflix/User.java
+++ src/netflix/User.java
@@ -5,4 +5,18 @@ public class User {
     private String strNickname;
     private int intAge;
     private boolean blnMaturity; 
+
+    /**
+     * 
+     * @param nickname: the user's nickname 
+     * @param age: the user's age
+     * @param mature: the user's maturity 
+     */
+    public User(String nickname, int age, boolean mature) {
+        strNickname = nickname;
+        intAge = age;
+        blnMaturity = mature;
+    }
+
+
 }
